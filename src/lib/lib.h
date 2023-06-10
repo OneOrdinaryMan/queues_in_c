908bc3a209036e8bb07f2cc7d73a9f2fb1a84d97
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-10 16:21:50 +0530

    Prototyped in lib.h

---
+++ src/lib/lib.h
@@ -0,0 +1,22 @@
+#ifndef LIB_H
+#define LIB_H
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  int value;
+  struct Node *next;
+};
+typedef struct Node node;
+typedef struct {
+  node *head;
+  node *tail;
+  int size;
+} queue;
+queue create_queue();
+node *create_node(int);
+int peek(queue *);
+void enqueue(queue *, int);
+int dequeue(queue *);
+void delete_queue(queue *);
+#endif
