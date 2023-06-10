ce9e5957fad0787167789d42a68512c8edd5438f
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-10 16:58:02 +0530

    Added the newline characters in main.c

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@ int main() {
   queue input_queue = create_queue();
   enqueue(&input_queue, 5);
   enqueue(&input_queue, 6);
-  printf("%d", dequeue(&input_queue));
-  printf("%d", peek(&input_queue));
+  printf("%d\n", dequeue(&input_queue));
+  printf("%d\n", peek(&input_queue));
   delete_queue(&input_queue);
   return 0;
 }
