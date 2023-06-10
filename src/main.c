79111bb9f17e093e6c9ce2c71aee15e1f9717bd1
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-10 16:56:35 +0530

    Formed main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
+#include "lib/lib.h"
 #include <stdio.h>
 int main() {
-  printf("Hello, World!\n");
+  queue input_queue = create_queue();
+  enqueue(&input_queue, 5);
+  enqueue(&input_queue, 6);
+  printf("%d", dequeue(&input_queue));
+  printf("%d", peek(&input_queue));
+  delete_queue(&input_queue);
   return 0;
 }
